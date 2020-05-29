af5446240d91ad158c7f78fc157fa95e3f9a2c3d
tv32541 <aravindnass@gmail.com>
2020-05-29 12:24:26 +0530

    Updates

---
+++ precache-manifest.89395a714d79bd95375c183924f91271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c46e3dd082d249378158e5e12715c2b",
+    "url": "/react-todoapp/index.html"
+  },
+  {
+    "revision": "2a09660bb2f0ce06bfc0",
+    "url": "/react-todoapp/static/css/main.75b8bfd2.chunk.css"
+  },
+  {
+    "revision": "b84ff76823842a15d57c",
+    "url": "/react-todoapp/static/js/2.a5b3c9e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todoapp/static/js/2.a5b3c9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a09660bb2f0ce06bfc0",
+    "url": "/react-todoapp/static/js/main.d8ab2575.chunk.js"
+  },
+  {
+    "revision": "1db5caaaf972ffdfb712",
+    "url": "/react-todoapp/static/js/runtime-main.fbf26d7a.js"
+  }
+]);
